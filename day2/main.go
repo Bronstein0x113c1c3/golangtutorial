d3908cde0a012ff6b435894a8e0324b41679ca2b
Bronstein0x113c1c3 <tuananhsmart123456789@gmail.com>
2022-09-26 05:57:54 +0700

    some data types examples

--- day2/main.go
+++ day2/main.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	var a bool = true
+	var a int8 = -120
 	fmt.Printf("%v %T \n", a, a)
 	fmt.Println("Hello Playground")
 }
@@ -13,6 +13,8 @@ Primitive data types:
 	1.Booleans
 	2.Numerics
 		-Integers
+			signed integers: int8 int16 int32 int64
+			unsigned integers: uint8 uint16 uint32 uint64
 		-Float
 		-Complex
 	3.Text/File
