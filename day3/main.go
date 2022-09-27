b701539448c847cc0dbede643a155e5cc6af8564
Bronstein0x113c1c3 <tuananhsmart123456789@gmail.com>
2022-09-27 09:50:46 +0700

    enumeration

--- day3/main.go
+++ day3/main.go
@@ -4,16 +4,27 @@ import (
 	"fmt"
 )
 
-const a = 42
+const (
+	red    = 0
+	yellow = 1
+	blue   = 2
+	black  = 3
+)
 
 func main() {
-	const a = 12
+	// const a = 12
 	/*
 		you cannot assign a const: i = 15 is illegal
 	*/
 	/*
 		inner-scope variable is more prioritized
 	*/
-	fmt.Printf("%v, %T \n", a, a)
-	fmt.Println("Hello Playground")
+	// fmt.Printf("%v, %T \n", a, a)
+	// fmt.Println("Hello Playground")
+
+	fmt.Printf("%v, %T \n", red, red)
+	fmt.Printf("%v, %T \n", yellow, yellow)
+	fmt.Printf("%v, %T \n", blue, blue)
+	fmt.Printf("%v, %T \n", black, black)
+
 }
