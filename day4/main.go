5d69ce13cdb7ba9b92753f3087d9cc2d2e855ea1
Bronstein0x113c1c3 <tuananhsmart123456789@gmail.com>
2022-09-27 20:53:02 +0700

    copy an array using assignment operator

--- day4/main.go
+++ day4/main.go
@@ -3,11 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	points := [3]string{"Alex", "Tom", "Yuh"}
+	a := [3]int{2, 5, 10}
 	// points[0] =
 	// points[1] =
 	// points[2] =
-	fmt.Printf("%v, %T \n", points, points)
-	fmt.Printf("%v, %T \n", points[2], points[2])
-	fmt.Println(len(points[1]))
+	// b := a  // copy an array
+	b := &a //copy the address of a to b
+	b[1] = 20
+	fmt.Println(a)
+	fmt.Println(b)
 }
