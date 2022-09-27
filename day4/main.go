7a2fcc546c570aa49afb165c42c0fbf395f98c4b
Bronstein0x113c1c3 <tuananhsmart123456789@gmail.com>
2022-09-27 21:26:11 +0700

    copy elements using indices

--- day4/main.go
+++ day4/main.go
@@ -3,13 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	a := [3]int{2, 5, 10}
+	// a := []int{2, 5, 10, 12, 45, 50}
+	a := make([]int, 10)
 	// points[0] =
 	// points[1] =
 	// points[2] =
 	// b := a  // copy an array
-	b := &a //copy the address of a to b
-	b[1] = 20
-	fmt.Println(a)
+	b := a[3:6]
+	// b:= a[x:y] copy from a from index x to index y-1
 	fmt.Println(b)
 }
