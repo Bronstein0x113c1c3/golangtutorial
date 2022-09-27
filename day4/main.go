4d637727a891ae13c62ffa328408f5379009352c
Bronstein0x113c1c3 <tuananhsmart123456789@gmail.com>
2022-09-27 20:45:11 +0700

    how to get manipulate elements through index

--- day4/main.go
+++ day4/main.go
@@ -4,5 +4,10 @@ import "fmt"
 
 func main() {
 	points := [3]int{7, 10, 5}
+	points[0] = 9
+	points[1] = 10
+	points[2] = 5
 	fmt.Printf("%v, %T \n", points, points)
+	fmt.Printf("%v, %T \n", points[2], points[2])
+
 }
