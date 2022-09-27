f586d7382d828bec1606d154641081c801b86857
Bronstein0x113c1c3 <tuananhsmart123456789@gmail.com>
2022-09-27 21:32:08 +0700

    append a slice

--- day4/main.go
+++ day4/main.go
@@ -3,13 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	// a := []int{2, 5, 10, 12, 45, 50}
 	a := make([]int, 10)
 	// points[0] =
 	// points[1] =
 	// points[2] =
 	// b := a  // copy an array
-	b := a[3:6]
+	// b := a[3:6]
 	// b:= a[x:y] copy from a from index x to index y-1
-	fmt.Println(b)
+	a = append(a, 1)
+	fmt.Printf("a: %v %v %v", a, len(a), cap(a))
+	a = append(a, 2)
+	fmt.Printf("a: %v %v %v", a, len(a), cap(a))
 }
