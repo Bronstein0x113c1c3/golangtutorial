4ba7a9c455756310b2bae8bfbe73bc86cbe484af
Bronstein0x113c1c3 <tuananhsmart123456789@gmail.com>
2023-01-10 00:40:32 +0700

    concurrency with pipeline

---
+++ day8_pascal_triangle_pipelining/main.go
@@ -0,0 +1,59 @@
+package main
+
+import "fmt"
+
+func result_chan_pipeline(number_chan chan int, result_chan chan int) {
+	var a, b int
+	go func() {
+		for {
+			a = <-number_chan
+			b = <-number_chan
+			result_chan <- a + b
+		}
+	}()
+}
+
+func pascal_triangle_concurrency(x []int) []int {
+	// var b, c int
+	a := x
+
+	number_chan := make(chan int, 3)
+	result_chan := make(chan int, 1000000)
+	// go func() {
+	// 	for {
+	// 		b = <-number_chan
+	// 		c = <-number_chan
+	// 		result_chan <- b + c
+	// 	}
+	// }()
+	result_chan_pipeline(number_chan, result_chan)
+	for i := 1; i <= len(a)-1; i++ {
+		number_chan <- a[i-1]
+		number_chan <- a[i]
+		if i >= 3 {
+			a[i-2] = <-result_chan
+		}
+
+	}
+	return a
+}
+
+func main() {
+
+	/*if the length of arr is x, Pascal Triangle could reach x-2 units
+	, with the lowest unit start from index 0 => highest index is x-3!
+	*/
+	a := make([]int, 18)
+	a[0] = 1
+	a[1] = 0
+
+	fmt.Println(a[0])
+	for i := 1; i <= len(a)-3; i++ {
+		a = pascal_triangle_concurrency(a)
+		for j := 0; j <= i; j++ {
+			fmt.Print(a[j], " ")
+		}
+		fmt.Println()
+	}
+
+}
