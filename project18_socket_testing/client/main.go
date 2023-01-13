8dabc09d7ab4aeefe3354ffe77ce1104cad79c6d
Bronstein0x113c1c3 <tuananhsmart123456789@gmail.com>
2023-01-13 09:54:20 +0700

    some comments and explainations

--- project18_socket_testing/client/main.go
+++ project18_socket_testing/client/main.go
@@ -17,13 +17,17 @@ func main() {
 	if err != nil {
 		log.Fatalf("Error encountered: %v", err)
 	}
+	//add waitgroup to hold the goroutines from being halted
 	wg.Add(1)
+	//split the connection into 2 distinct goroutines to make it run more efficiently
 	go readmsg(conn)
 	go writemsg(conn)
+	//wait for the waitgroup to be done!
 	wg.Wait()
 
 }
 func readmsg(c net.Conn) {
+	//get it in while loop to hold it!
 	for {
 		b := make([]byte, 1024)
 		c.Read(b)
@@ -33,6 +37,7 @@ func readmsg(c net.Conn) {
 	}
 }
 func writemsg(c net.Conn) {
+	//also, but when we want to quit, just say "quit", the program halted!
 	for {
 		reader := bufio.NewReader(os.Stdin)
 		text, _ := reader.ReadString('\n')
