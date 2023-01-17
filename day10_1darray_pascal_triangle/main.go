be3b6283df8acc32d0c8c53c2db0a1dcf6c570b1
Bronstein0x113c1c3 <tuananhsmart123456789@gmail.com>
2023-01-17 10:53:12 +0700

    golang pascal triagle dp

---
+++ day10_1darray_pascal_triangle/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := make([]int, 0)
+	a = append(a, 1)
+	a = append(a, 0)
+	for i := 1; i <= 5; i++ {
+		prev_len := len(a)
+		a = append(a, 1)
+		for j := 1; j <= i; j++ {
+			a = append(a, a[prev_len-(i+1)+j]+a[prev_len-(i+1)+j-1])
+		}
+		a = append(a, 0)
+	}
+	fmt.Println(a)
+}
