941d52537ebf0e69f5bf2c091c1d01d7183f6a98
Bronstein0x113c1c3 <tuananhsmart123456789@gmail.com>
2023-10-24 14:30:44 +0700

    i have found something...

--- networking_go/day1_server/main.go
+++ networking_go/day1_server/main.go
@@ -1,24 +1,109 @@
 package main
 
 import (
-	"log"
+	"context"
+	"fmt"
 	"net"
+	"os"
+	"os/signal"
 )
 
-func handleconn(c net.Conn) {
-	for {
-		c.Write([]byte("Bonjour! \n"))
-	}
-	c.Close()
-}
+//method 1
+// func handleConnection(conn net.Conn) {
+// 	// Create a channel to listen for OS signals
+// 	sigs := make(chan os.Signal, 1)
 
+// 	// Register the channel to receive SIGINT and SIGTERM signals
+// 	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+
+// 	go func() {
+// 		// Wait for a signal
+// 		sig := <-sigs
+
+// 		fmt.Println()
+// 		fmt.Println("Received signal:", sig)
+
+// 		// Close the connection
+// 		conn.Close()
+
+// 		fmt.Println("Connection closed")
+// 	}()
+
+// 	// Handle the connection here
+// 	for {
+// 		conn.Write([]byte("Bonjour!"))
+// 	}
+// }
+
+//method 2
+
+// func handleConnection(conn net.Conn) {
+// 	// Create a context that cancels when an interrupt signal is received
+// 	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
+// 	defer stop()
+
+// 	// Create a channel to simulate connection data
+// 	data := make(chan string)
+
+// 	go func() {
+// 		// Simulate receiving data
+// 		for {
+// 			conn.Write([]byte("Bonjour!"))
+// 		}
+// 	}()
+
+// 	for {
+// 		select {
+// 		case <-ctx.Done():
+// 			// Close the connection
+// 			conn.Close()
+
+// 			fmt.Println("Connection closed")
+// 			return
+// 		case msg := <-data:
+// 			// Handle the connection data
+// 			fmt.Println(msg)
+// 		}
+// 	}
+// }
+
+func handleConnection(conn net.Conn) {
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
+	go func(ctx context.Context) {
+		select {
+		case <-ctx.Done():
+			fmt.Println("Done!")
+			conn.Write([]byte("Goodbye! \n"))
+			conn.Close()
+			cancel()
+			return
+		default:
+			for {
+				conn.Write([]byte("Hello!"))
+			}
+		}
+	}(ctx)
+	go func() {
+		<-ctx.Done()
+	}()
+}
 func main() {
-	lis, err := net.Listen("tcp", ":3000")
+	// Set up a TCP listener
+	listener, err := net.Listen("tcp", ":3000")
 	if err != nil {
-		log.Fatal(err)
+		fmt.Println("Error setting up TCP listener:", err)
+		os.Exit(1)
 	}
+
+	// Accept connections in a loop
 	for {
-		conn, _ := lis.Accept()
-		go handleconn(conn)
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection:", err)
+			break
+		}
+
+		go handleConnection(conn)
 	}
 }
